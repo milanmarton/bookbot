d85dcb3de6882ef893130c5afe4f6a7b579b427e
milan <martonmilan@proton.me>
2024-03-30 02:19:06 +0100

    updated file reading functionality

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ import sys
 
 def main():
     path_to_file = sys.argv[1]
-    with open(path_to_file) as f:
-        file_contents = f.read()
-        print(file_contents)
+    text = get_text(path_to_file)
+    print(text)
+
+
+def get_text(path):
+    with open(path) as f:
+        return f.read()
 
 
 main()
