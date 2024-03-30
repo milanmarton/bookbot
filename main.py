b51dd66876a853062b092e79a165ba8136135173
milan <martonmilan@proton.me>
2024-03-30 02:27:55 +0100

    added unique letter counting

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ def main():
     path_to_file = sys.argv[1]
     text = get_text(path_to_file)
     word_count = calc_word_count(text)
-    print(word_count)
+    letter_count = calc_letter_count(text)
+    print(letter_count)
 
 def get_text(path):
     with open(path) as f:
@@ -13,5 +14,15 @@ def get_text(path):
 def calc_word_count(text):
     words = text.split()
     return len(words)
+
+def calc_letter_count(text):
+    lowered_text = text.lower()
+    letter_count = {}
+    for c in lowered_text:
+        if c in letter_count:
+            letter_count[c] += 1
+        else:
+            letter_count[c] = 1
+    return letter_count
 
 main()
