4bd182a9175f36ed55d30d4330fa563e5c592d41
milan <martonmilan@proton.me>
2024-03-30 02:21:53 +0100

    added word counting

--- main.py
+++ main.py
@@ -3,12 +3,15 @@ import sys
 def main():
     path_to_file = sys.argv[1]
     text = get_text(path_to_file)
-    print(text)
-
+    word_count = calc_word_count(text)
+    print(word_count)
 
 def get_text(path):
     with open(path) as f:
         return f.read()
 
+def calc_word_count(text):
+    words = text.split()
+    return len(words)
 
 main()
